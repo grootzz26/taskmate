8ddf948b600d3a2494a91d2409794ba0b36cb806
sargunaraj <gunasalu26@gmail.com>
2022-03-10 07:37:32 +0530

    add gunicorn

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
@@ -121,3 +121,5 @@ LOGIN_REDIRECT_URL = 'todolist'
 LOGIN_URL = 'login'
 
 django_heroku.settings(locals())
+
+
